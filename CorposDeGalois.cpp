d46e2fcb405973ec62ba61c0ea2088bedee7c3ce
Bazchh <mikael.vidal@gmail.com>
2023-03-04 20:48:42 -0300

    Funçã de produto de um corpo finito de galois

--- CorposDeGalois.cpp
+++ CorposDeGalois.cpp
@@ -1,41 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **CorpoDeGaloisPorSoma(int primo, int **m){
+int **corpoDeGaloisPorProduto(int primo, int **m)
+{
     int i;
     int j;
-    int k;
 
-    for(i = 0; i < primo; i++){
-        for(j = 0; j < primo; j++){
-    
-        m[i][j] = i+j;
+    for (i = 0; i < primo; i++)
+    {
+        for (j = 0; j < primo; j++)
+        {
 
-    if(i+j >= primo){
-        m[i][j] = (i+j)%primo;
+            m[i][j] = i * j;
+
+            if (i * j >= primo)
+            {
+                m[i][j] = (i * j) % primo;
             }
         }
     }
- return m;
+    return m;
 }
 
-int main(){
-int i;
-int primo = 42;
-int **m;
-m = (int**)malloc(primo*sizeof(int*));
-    for(i = 0; i < primo; i++){
-        m[i] = (int*)malloc(primo*sizeof(int));
+int **CorpoDeGaloisPorSoma(int primo, int **m)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < primo; i++)
+    {
+        for (j = 0; j < primo; j++)
+        {
+
+            m[i][j] = i + j;
+
+            if (i + j >= primo)
+            {
+                m[i][j] = (i + j) % primo;
+            }
+        }
     }
-m = CorpoDeGaloisPorSoma(primo,m);
+    return m;
+}
 
-int j;
+int main()
+{
+    int i;
+    int primo = 7;
+    int **m;
+    m = (int **)malloc(primo * sizeof(int *));
+    for (i = 0; i < primo; i++)
+    {
+        m[i] = (int *)malloc(primo * sizeof(int));
+    }
+    m = CorpoDeGaloisPorSoma(primo, m);
 
-for (i = 0; i < primo;i++){
-    for (j = 0; j < primo;j++){
-    printf("%5i", m[i][j]);
+    int j;
+
+    m = corpoDeGaloisPorProduto(primo, m);
+
+    for (i = 0; i < primo; i++)
+    {
+        for (j = 0; j < primo; j++)
+        {
+            printf("%5i", m[i][j]);
         }
         printf("\n");
     }
-
 }
