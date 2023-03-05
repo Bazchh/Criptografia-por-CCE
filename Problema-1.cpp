9eb06d0c3f7218970ab0737bc8afce0254ea53e0
Bazchh <mikael.vidal@gmail.com>
2023-03-05 11:58:29 -0300

    Add comentarios

--- Problema-1.cpp
+++ Problema-1.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "CorposDeGalois.cpp"
+
+/*
+
+Problema 1 proposto:
+
+Dentro do campo de ordem p = 47,
+encontrar todos os pontos pertencentes a curva eliptica
+abaixo:
+
+y² = x³ + 22x + 15
+
+*/
+
 int main()
 {
     int p;
